74f4c171453604d26bce47ac2ca5e26483d6f263
mohanababug <124085229+mohanababug@users.noreply.github.com>
2023-04-10 15:56:06 +0100

    Add files via upload

---
+++ src/test/java/com/mg/ocp/api/demoapis/DemoapisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mg.ocp.api.demoapis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoapisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
